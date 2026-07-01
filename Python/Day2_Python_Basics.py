49d71b38a017cbd5d0f5c51464abfd838bfab662
rasika-data-analyst <pavrasika@gmail.com>
2026-07-02 00:17:56 +0530

    Day 2 - Python Operators Completed
    ✅ Variables
    ✅ Data Types
    ✅ print() and input()
    ✅ Type Conversion
    ✅ Arithmetic Operators
    ✅ Comparison Operators
    ✅ Logical Operators
    ✅ Assignment Operators
    ✅ Membership Operators
    ✅ Identity Operators
    ✅ Practice questions
    ✅ Interview questions
    ✅ Mini assignment

---
+++ Python/Day2_Python_Basics.py
@@ -0,0 +1,924 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "45d31bce-6ae8-483a-8d79-035909485947",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Rasika\n"
+     ]
+    }
+   ],
+   "source": [
+    "#Q1.Create a variable called name and store your name.\n",
+    "\n",
+    "name = \"Rasika\"\n",
+    "\n",
+    "print(name)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "a90704f0-b39c-406b-bc14-ab795916b9f4",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Pune\n"
+     ]
+    }
+   ],
+   "source": [
+    "#Create a variable called city.\n",
+    "\n",
+    "city = 'Pune'\n",
+    "\n",
+    "print(city)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "816bc5b9-5ae4-4c72-a0bc-42f850b2f4e3",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Rasika\n",
+      "Pune\n",
+      "150000\n"
+     ]
+    }
+   ],
+   "source": [
+    "#Q3Create a variable called salary. Q4Print all three variables.\n",
+    "\n",
+    "Salary = 150000\n",
+    "print(name)\n",
+    "print(city)\n",
+    "print(Salary)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 12,
+   "id": "47f3fbbb-6042-4217-9a5a-784a4268aeec",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#Q5Create three variables in one line.\n",
+    "\n",
+    "name, city , Salary = ['Rasika','Pune',50000]"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "id": "fcd3d709-2c40-4750-bca2-522ca88092a6",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "10\n",
+      "20\n",
+      "30\n",
+      "100\n",
+      "100\n",
+      "100\n"
+     ]
+    }
+   ],
+   "source": [
+    "#Q5Create three variables in one line.\n",
+    "\n",
+    "a, b, c = 10, 20, 30\n",
+    "\n",
+    "print(a)\n",
+    "\n",
+    "print(b)\n",
+    "\n",
+    "print(c)\n",
+    "\n",
+    "\n",
+    "#Q6Assign the same value to three variables.\n",
+    "x = y = z = 100\n",
+    "\n",
+    "print(x)\n",
+    "\n",
+    "print(y)\n",
+    "\n",
+    "print(z)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 15,
+   "id": "795b8772-aabf-4234-810e-e414a8067c88",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Rasika\n",
+      "Pune\n",
+      "Data Analyst\n"
+     ]
+    }
+   ],
+   "source": [
+    "Name = 'Rasika'\n",
+    "\n",
+    "City = 'Pune'\n",
+    "\n",
+    "Profession = 'Data Analyst'\n",
+    "\n",
+    "\n",
+    "\n",
+    "print(name)\n",
+    "print(city)\n",
+    "print(Profession)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 16,
+   "id": "7b2eb086-f333-452f-9f6d-baff4313baa3",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "30\n",
+      "<class 'int'>\n"
+     ]
+    }
+   ],
+   "source": [
+    "#Q1 Create a variable called age with value 30. Print its value and data type.\n",
+    "\n",
+    "age = 30\n",
+    "print(age)\n",
+    "print(type(age))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 17,
+   "id": "4eeb1f81-617d-4566-92f0-1a58fa101383",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "75000.5\n",
+      "<class 'float'>\n"
+     ]
+    }
+   ],
+   "source": [
+    "#Q2Create a variable called salary with value 75000.50.Print its value and data type.\n",
+    "\n",
+    "salary = 75000.50\n",
+    "print(salary)\n",
+    "print(type(salary))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 18,
+   "id": "b41245f8-f89e-4032-a8b4-9ee1d8eb499d",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "TCS\n",
+      "<class 'str'>\n"
+     ]
+    }
+   ],
+   "source": [
+    "#Q3Create a variable called company with value \"TCS\".Print its value and data type.\n",
+    "company = \"TCS\"\n",
+    "print(company)\n",
+    "print(type(company))\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 19,
+   "id": "0a3ad943-2f05-4b65-a391-9d25e24166ce",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "True\n",
+      "<class 'bool'>\n"
+     ]
+    }
+   ],
+   "source": [
+    "#Q4Create a variable called is_employee with value True.Print its value and data type.\n",
+    "is_employee = True \n",
+    "print(is_employee)\n",
+    "print(type(is_employee))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 21,
+   "id": "4687207c-672f-4830-95ca-a4e4e5c42d3c",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Rasika\n",
+      "28\n",
+      "150000.5\n",
+      "Pune\n",
+      "True\n",
+      "<class 'str'>\n",
+      "<class 'int'>\n",
+      "<class 'float'>\n",
+      "<class 'str'>\n",
+      "<class 'bool'>\n"
+     ]
+    }
+   ],
+   "source": [
+    "name = \"Rasika\"\n",
+    "\n",
+    "age = 28\n",
+    "\n",
+    "salary = 150000.50\n",
+    "\n",
+    "city = \"Pune\"\n",
+    "\n",
+    "is_working = True\n",
+    "\n",
+    "print(name)\n",
+    "print(age)\n",
+    "print(salary)\n",
+    "print(city)\n",
+    "print(is_working)\n",
+    "\n",
+    "print(type(name))\n",
+    "print(type(age))\n",
+    "print(type(salary))\n",
+    "print(type(city))\n",
+    "print(type(is_working))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 22,
+   "id": "342ee89b-b2dd-490e-a53d-003da8d8b38e",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Name: Rasika\n",
+      "City: Pune\n",
+      "Salary: 150000\n"
+     ]
+    }
+   ],
+   "source": [
+    "name = \"Rasika\"\n",
+    "city = \"Pune\"\n",
+    "salary = 150000\n",
+    "\n",
+    "print(\"Name:\", name)\n",
+    "print(\"City:\", city)\n",
+    "print(\"Salary:\", salary)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 23,
+   "id": "06d1c60f-20f0-4a33-9113-6f76d72249e7",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your name:  Rasika\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello Rasika\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Q1 :Take your name as input.Print:Hello Rasika\n",
+    "\n",
+    "name = input('Enter your name: ')\n",
+    "\n",
+    "print(\"Hello\" , name)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 25,
+   "id": "814b5978-b606-44f5-bedd-41c676241f45",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your city:  Pune\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Welcome to  Pune\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Q2 Take your city as input.\n",
+    "\n",
+    "city = input('Enter your city: ')\n",
+    "print (\"Welcome to \" , city)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 29,
+   "id": "c4a3acca-b9bc-4def-adc9-fa6c337502b5",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your age:  28\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Your age is  28\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Q3 Take your age as input.\n",
+    "\n",
+    "age = int(input(\"Enter your age: \" ))\n",
+    "print(\"Your age is \",age )"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 32,
+   "id": "e035040c-93a4-42f8-935e-9bd9f5f54aef",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your salary:  150000.50\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Salary:  150000.5\n"
+     ]
+    }
+   ],
+   "source": [
+    "#Q4 Take salary as input.\n",
+    "salary = float(input(\"Enter your salary: \"))\n",
+    "print (\"Salary: \" , salary)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 33,
+   "id": "193d4b3b-dc45-4ada-948b-de6d3595fad7",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your name:  Rasika\n",
+      "Enter your age:  28\n",
+      "Enter your city:  Pune\n",
+      "Enter your course:  Python\n",
+      "Enter your marks:  95.\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "------ Student Information ------\n",
+      "Name:  Rasika\n",
+      "Age :   28\n",
+      "City :  Pune\n",
+      "Course :  Python\n",
+      "Marks :  95.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "Name = input('Enter your name: ')\n",
+    "Age =  int(input(\"Enter your age: \" ))\n",
+    "City = input('Enter your city: ')\n",
+    "Course = input('Enter your course: ')\n",
+    "Marks = float(input(\"Enter your marks: \" ))\n",
+    "\n",
+    "print (\"------\" , \"Student Information\" , \"------\")\n",
+    "\n",
+    "print(\"Name: \" , Name)\n",
+    "print(\"Age :  \", Age)\n",
+    "print(\"City : \", City)\n",
+    "print(\"Course : \", Course)\n",
+    "print(\"Marks : \" , Marks) "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 34,
+   "id": "15b6e5a0-db5a-4157-9230-a61400374d02",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "40\n"
+     ]
+    }
+   ],
+   "source": [
+    "a = 25\n",
+    "b = 15\n",
+    "\n",
+    "print(a + b)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 35,
+   "id": "ed15d4c2-17ab-4303-bc1a-5a891603a854",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "32\n"
+     ]
+    }
+   ],
+   "source": [
+    "a = 50\n",
+    "b = 18\n",
+    "\n",
+    "print(a - b)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 36,
+   "id": "5d65c5f1-7896-47ab-97e9-a74f3e4d2355",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "600\n"
+     ]
+    }
+   ],
+   "source": [
+    "price = 75\n",
+    "quantity = 8\n",
+    "\n",
+    "print(price * quantity)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 37,
+   "id": "c790e674-0cc0-4612-9101-cea8a29d3ce4",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "6.25\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(25 / 4)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 38,
+   "id": "84aef776-2f03-4a5a-94dd-8231b1a0e573",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "6\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(25 // 4)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 39,
+   "id": "c3785af5-8722-4120-8667-c437d8fd51d3",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "1\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(25 % 4)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 40,
+   "id": "94396f01-7802-40c2-97f6-1c9ce11005e5",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "81\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(3 ** 4)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 41,
+   "id": "3b5dedb4-76c5-4cad-bbe7-dd90915e38a7",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter the num1:   20\n",
+      "Enter the num2:   30\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Addition:  50\n",
+      "subtraction:  -10\n",
+      "Multiplication:  600\n",
+      "Division:  0.6666666666666666\n",
+      "Floor Division:  0\n",
+      "Modulus:  20\n",
+      "Power:  1073741824000000000000000000000000000000\n"
+     ]
+    }
+   ],
+   "source": [
+    "num1 = int(input(\"Enter the num1:  \"))\n",
+    "num2 = int(input(\"Enter the num2:  \"))\n",
+    "\n",
+    "print(\"Addition: \" , num1+num2)\n",
+    "print(\"subtraction: \" , num1-num2)\n",
+    "print(\"Multiplication: \", num1*num2) \n",
+    "print(\"Division: \" , num1/num2)\n",
+    "print(\"Floor Division: \" , num1//num2) \n",
+    "print(\"Modulus: \" , num1%num2) \n",
+    "print(\"Power: \" ,num1**num2)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 43,
+   "id": "062bb50a-5176-4f48-9980-bd98ee33609d",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "True\n",
+      "True\n",
+      "False\n",
+      "True\n",
+      "True\n",
+      "False\n",
+      "True\n",
+      "False\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Practice Questions\n",
+    "\n",
+    "### Q1\n",
+    "\n",
+    "print(15 == 15)\n",
+    "\n",
+    "### Q2\n",
+    "print(25 != 30)\n",
+    "\n",
+    "### Q3\n",
+    "print(40 > 100)\n",
+    "\n",
+    "### Q4\n",
+    "print(75 < 120)\n",
+    "\n",
+    "### Q5\n",
+    "print(100 >= 100)\n",
+    "\n",
+    "### Q6\n",
+    "print(55 <= 20)\n",
+    "### Q7\n",
+    "age = 18\n",
+    "\n",
+    "print(age >= 18)\n",
+    "### Q8\n",
+    "marks = 92\n",
+    "\n",
+    "print(marks < 35)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 44,
+   "id": "581ab967-393e-4838-aafc-6450461d8e82",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter the age:  20\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Age == 18 : False\n",
+      "Age != 18 : True\n",
+      "Age > 18  : True\n",
+      "Age < 18  : False\n",
+      "Age >= 18 : True\n",
+      "Age <= 18 : False\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Coding Practice Write a program that:\n",
+    "\n",
+    "age =int(input(\"Enter the age: \" ))\n",
+    "\n",
+    "print(\"Age == 18 :\", age == 18)\n",
+    "print(\"Age != 18 :\", age != 18)\n",
+    "print(\"Age > 18  :\", age > 18)\n",
+    "print(\"Age < 18  :\", age < 18)\n",
+    "print(\"Age >= 18 :\", age >= 18)\n",
+    "print(\"Age <= 18 :\", age <= 18)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 45,
+   "id": "ff87f20c-e014-4aac-89f1-b66df54e15ca",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "15\n",
+      "12\n",
+      "30\n",
+      "6.0\n",
+      "True\n",
+      "False\n",
+      "True\n",
+      "True\n",
+      "True\n"
+     ]
+    }
+   ],
+   "source": [
+    "## Q1\n",
+    "x = 10\n",
+    "\n",
+    "x += 5\n",
+    "\n",
+    "print(x)\n",
+    "\n",
+    "\n",
+    "## Q2\n",
+    "x = 20\n",
+    "\n",
+    "x -= 8\n",
+    "\n",
+    "print(x)\n",
+    "## Q3\n",
+    "\n",
+    "x = 5\n",
+    "\n",
+    "x *= 6\n",
+    "\n",
+    "print(x)\n",
+    "\n",
+    "## Q4\n",
+    "\n",
+    "x = 30\n",
+    "\n",
+    "x /= 5\n",
+    "\n",
+    "print(x)\n",
+    "\n",
+    "## Q5\n",
+    "name = \"Python\"\n",
+    "\n",
+    "print(\"P\" in name)\n",
+    "\n",
+    "## Q6\n",
+    "\n",
+    "name = \"Python\"\n",
+    "\n",
+    "print(\"z\" in name)\n",
+    "\n",
+    "## Q7\n",
+    "course = \"Data Analyst\"\n",
+    "\n",
+    "print(\"SQL\" not in course)\n",
+    "\n",
+    "## Q8\n",
+    "a = 50\n",
+    "b = a\n",
+    "\n",
+    "print(a is b)\n",
+    "\n",
+    "## Q9\n",
+    "a = 10\n",
+    "b = 20\n",
+    "\n",
+    "print(a is not b)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 51,
+   "id": "3c447c91-ca5f-4376-ac15-8d3acb1ce62d",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter the marks:  30\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "35\n",
+      "30\n",
+      "150\n",
+      "30.0\n"
+     ]
+    },
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter the course:  Python\n"
+     ]
+    },
+    {
+     "data": {
+      "text/plain": [
+       "True"
+      ]
+     },
+     "execution_count": 51,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# Coding Practice Write a program that: 3. Prints the updated marks after each operation.\n",
+    "\n",
+    "marks = int(input(\"Enter the marks: \"))\n",
+    "marks += 5\n",
+    "print(marks)\n",
+    "marks -= 5\n",
+    "print(marks)\n",
+    "marks *= 5\n",
+    "print(marks)\n",
+    "marks /= 5\n",
+    "print(marks)\n",
+    "\n",
+    "\n",
+    "#Take a course name as input and check:\n",
+    "\n",
+    "course = input(\"Enter the course: \")\n",
+    "\"Python\" in course\n"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "6b22709f-5014-457c-870f-59957e4846d3",
+   "metadata": {},
+   "source": [
+    "| Topic                  | Difference                                          |\n",
+    "| ---------------------- | --------------------------------------------------- |\n",
+    "| `=` vs `==`            | `=` assigns value, `==` compares values             |\n",
+    "| `==` vs `is`           | `==` compares values, `is` compares object identity |\n",
+    "| `/` vs `//`            | `/` gives float, `//` gives whole-number quotient   |\n",
+    "| `in` vs `not in`       | Exists vs Does Not Exist                            |\n",
+    "| `and` vs `or`          | Both True vs At Least One True                      |\n",
+    "| `int` vs `float`       | Whole number vs Decimal number                      |\n",
+    "| `print()` vs `input()` | Display output vs Take user input                   |\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:base] *",
+   "language": "python",
+   "name": "conda-base-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
